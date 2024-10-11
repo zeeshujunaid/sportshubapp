55be79c611a319951f0f4927f4c56a8107cec20b
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2024-10-11 05:25:41 +0500

    drawer working not finished

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,10 @@
-import { Stack } from "expo-router";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Drawer } from 'expo-router/drawer';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <Drawer />
-        <Stack.Screen name="index" options={{ headerShown: false, }} />
-        <Stack.Screen name="signup" options={{ headerShown: false, }} />
-        <Stack.Screen name="login" options={{ headerShown: false, }} />
-        <Stack.Screen name="home" options={{ headerShown: false, }} />
-      </GestureHandlerRootView>
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+    <Drawer  />
+  </GestureHandlerRootView>
   );
 }
